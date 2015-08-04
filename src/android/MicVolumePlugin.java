0c8af509fb04415b335fa21110e5a904917e9058
mbpartido <mbpartido@gmail.com>
2015-08-04 14:38:32 +0200

    no message

--- src/android/MicVolumePlugin.java
+++ src/android/MicVolumePlugin.java
@@ -82,28 +82,6 @@ public class MicVolumePlugin extends CordovaPlugin
         returnObj.put("volume", Math.sqrt(amplitude));
        callbackContext.success(returnObj);
 
-AlertDialog alertDialog = new AlertDialog.Builder(
-                        AlertDialogActivity.this).create();
- 
-        // Setting Dialog Title
-        alertDialog.setTitle("Alert Dialog");
- 
-        // Setting Dialog Message
-        alertDialog.setMessage("Welcome to AndroidHive.info");
- 
-        // Setting Icon to Dialog
-        alertDialog.setIcon(R.drawable.tick);
- 
-        // Setting OK Button
-        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                // Write your code here to execute after dialog closed
-                Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
-                }
-        });
- 
-        // Showing Alert Message
-        alertDialog.show();
     }
 
     private void stop(CallbackContext callbackContext) {
